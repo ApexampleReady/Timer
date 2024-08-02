77fb6d952a5c256f8243cfd0df1a38bb4d8bcc50
Ting Kai Chiu <tingkai.chiu@gmail.com>
2024-08-02 09:00:36 +0800

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by ctk on 2024/8/2.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
